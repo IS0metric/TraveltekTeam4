fc7e3e03dad56f691a01ec9bffd54cc1b9ecff52
Ian Denev <contact@iandenev.com>
2017-03-25 18:01:39 +0000

    api key update - restricted to ubuntu server

--- innovation/js/innovation.js
+++ innovation/js/innovation.js
@@ -1,4 +1,4 @@
-var destination = "North Korea";
+var destination = "Puerto Quenzal";
 
 $('#video-container').html('<iframe id="ytplayer" type="text/html" width="720" height="405" src="https://www.youtube.com/embed/?listType=search&list=' + destination + ' travel" frameborder="0" allowfullscreen>');
 
