d6ad815a237fc716f5e70fa3a998ed78a5a9ad31
Ian Denev <contact@iandenev.com>
2017-03-25 16:43:31 +0000

    Hawaii + "travel" search results appearing now

---
+++ innovation/js/innovation.js
@@ -0,0 +1,3 @@
+var destination = "Hawaii";
+
+$('#video-container').html('<iframe id="ytplayer" type="text/html" width="720" height="405" src="https://www.youtube.com/embed/?listType=search&list=' + destination + ' travel" frameborder="0" allowfullscreen>');
